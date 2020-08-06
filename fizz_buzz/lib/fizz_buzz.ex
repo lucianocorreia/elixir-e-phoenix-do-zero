3d8db007abcda1d53c9bd8cc8fa414e34c1ca333
lucianocorreia <luciano.correia@gmail.com>
2020-08-06 20:36:44 -0300

    FizzBuzz

---
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -0,0 +1,29 @@
+defmodule FizzBuzz do
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
+  end
+
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(elemento) do
+    elemento
+    |> String.to_integer()
+    |> evaluate_number()
+  end
+
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
+end
